0bd24e5da8579f64728b463b12c798ec73767f27
Hasan Moboarak <hsncool8@gmail.com>
2021-03-16 14:39:26 +0600

    test

--- src/layouts/header/index.jsx
+++ src/layouts/header/index.jsx
@@ -1,11 +1,13 @@
 /** @jsx jsx */
 import { jsx } from "theme-ui";
 import { Fragment } from "react";
+import Anchor from "@shared/anchor";
 import { HeaderTop } from "./style";
 
 const Header = () => {
     return (
         <Fragment>
+            <Anchor>dfads</Anchor>
             <HeaderTop>Header</HeaderTop>
         </Fragment>
     );
